de5d299fddf8b381ed1b79d3b8b2e849e38e4382
pksingh786 <123pksingh786@gmail.com>
2022-02-11 19:16:38 +0530

    Enter

---
+++ 3_implementation/Src/ATM Banking.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+void login();
+void mainMenu();
+void checkBalance(float balance);
+float moneyDeposit(float balance);
+float moneyWithdraw(float balance);
+void menuExit();
+void errorMessage();
+
+
+
